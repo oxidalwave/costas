321179d6767a443a650e397b3805202ea80f6fcd
OxidalWave <github@oxidalwave.com>
2026-02-05 14:05:46 -0600

    Fix enumerate

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def main():
             Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
             draw = ImageDraw.Draw(Himage)
             schedule = statsapi.schedule(start_date='07/01/2018',end_date='07/31/2018',team=143,opponent=121)
-            for game, i in schedule:
+            for i, game in enumerate(schedule):
                 draw.text((10 * i, 120), f"{game['game_date']} Game {game['game_num']} - WP: {game['winning_pitcher']}, LP; {game['losing_pitcher']}", font = font24, fill = 0)
             epd.display_Partial(epd.getbuffer(Himage),0, 0, epd.width, epd.height)
         
