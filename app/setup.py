73a99b2396d2433c5546d1d64d63f188b6538026
OxidalWave <github@oxidalwave.com>
2026-02-05 12:12:37 -0600

    Remove package_dir from setup

--- app/setup.py
+++ app/setup.py
@@ -7,7 +7,6 @@ setup(
     name='waveshare-epd',
     description='Waveshare e-Paper Display',
     author='Waveshare',
-    package_dir={'': 'lib'},
     packages=['waveshare_epd'],
     install_requires=dependencies,
 )
