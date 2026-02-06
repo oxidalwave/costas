c6538daca18062577d075d4f19a02be1778934fa
OxidalWave <github@oxidalwave.com>
2026-02-06 09:17:03 -0600

    Shift scorebug around

--- app/ui/examples.py
+++ app/ui/examples.py
@@ -3,6 +3,19 @@ from typing import Dict
 from .core import Line, Text, View
 from .types import Style
 
+def teamScore(props: Dict[str, object]):
+    font = props["font"]
+    data = props["data"]
+
+    teamCode = data["teamCode"]
+    score = data["score"]
+
+
+    return View(
+        Text(teamCode, font=font),
+        Text(score, font=font, style=Style(align="end")),
+        style=Style(direction="row"),
+    )
 
 def scorebug(props: Dict[str, object]):
     font = props["font"]
@@ -13,36 +26,27 @@ def scorebug(props: Dict[str, object]):
     batter = data["atBat"]["batter"]
     pitcher = data["atBat"]["pitcher"]
 
-    row_style = Style(direction="row", gap=12)
-    column_style = Style(direction="column", gap=6)
-
     return View(
         View(
             View(
-                Text(away["teamCode"], font=font),
-                Text(away["score"], font=font, style=Style(align="end")),
-                style=row_style,
+                teamScore(props={"font": font, "data": away}),
+                teamScore(props={"font": font, "data": home}),
+                style=Style(direction="column"),
             ),
-            Line((0, 0), (400, 0)),
             View(
-                Text(home["teamCode"], font=font),
-                Text(home["score"], font=font, style=Style(align="end")),
-                style=row_style,
+                View(
+                    Text(batter["name"], font=font),
+                    Text(f"{batter['wpa+']} WPA+", font=font, style=Style(align="end")),
+                    style=Style(direction="row"),
+                ),
+                View(
+                    Text(pitcher["name"], font=font),
+                    Text(f"{pitcher['era']} ERA", font=font, style=Style(align="end")),
+                    style=Style(direction="row"),
+                ),
+                style=Style(direction="column"),
             ),
-            style=column_style,
+            style=Style(direction="row"),
         ),
-        View(
-            View(
-                Text(batter["name"], font=font),
-                Text(f"{batter['wpa+']} WPA+", font=font, style=Style(align="end")),
-                style=row_style,
-            ),
-            View(
-                Text(pitcher["name"], font=font),
-                Text(f"{pitcher['era']} ERA", font=font, style=Style(align="end")),
-                style=row_style,
-            ),
-            style=column_style,
-        ),
-        style=Style(direction="row", gap=24, padding=8),
+        View(style=Style(direction="row"))
     )
